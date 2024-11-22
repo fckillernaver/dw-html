c910e6952171f3490732c157e6e9ae965d014ace
fckillernaver <fckiller@naver.com>
2024-11-22 13:37:51 +0900

    init

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,14 +1,15 @@
 import {
-  createBrowserRouter,
+  // createBrowserRouter,
   createRoutesFromElements,
   Route,
+  createHashRouter,
 } from "react-router-dom";
 import HomeScreen from "./HomeScreen";
 import HTMLScreen from "./HTMLScreen";
 import HTMLTarget from "./HTMLTarget";
 import VScreen from "./VScreen";
 
-export const AppRouter = createBrowserRouter(
+export const AppRouter = createHashRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/" element={<HomeScreen />} />
